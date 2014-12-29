94e15ada50d99cbe0abec7a92d9d47b2bb69cd3e
FeriLupin <lord_aftermorfed@yahoo.com>
2014-12-29 17:49:19 +0700

    update paraf+ttd (type,size)

--- application/views/users/a.php
+++ application/views/users/a.php
@@ -13,8 +13,7 @@
 			<div class="control-group">
 				<label class="control-label">Tanda tangan</label>
 				<div class="controls">
-					<input type="file" name="userfile" id="userfile">
-					
+					<input type="file" name="userfile" id="userfile" onchange="cekttd(this);" accept="image/png">
 					<?php if($records){?>
 					<?php if($records['USERS_SIGNATURE']){?>
 					<input type="hidden" name="ttd_file" value="<?php echo $records['USERS_SIGNATURE'];?>" id="ttd">
@@ -25,7 +24,7 @@
 				
 				<label class="control-label">Paraf</label>
 				<div class="controls">
-					<input type="file" name="paraf" id="paraf">
+					<input type="file" name="paraf" id="paraf" onchange="cekparaf(this);" accept="image/png">
 					
 					<?php if($records){?>
 					<?php if($records['USERS_PARAF']){?>
@@ -45,6 +44,61 @@
 			</div>
 			</div>
 </form>
+<script type="text/javascript">
+   function cekttd(fieldObj)
+    {
+		var _URL = window.URL || window.webkitURL;
+        var FileName  = fieldObj.value;
+        var FileExt = FileName.substr(FileName.lastIndexOf('.')+1);
+
+        if (FileExt.toLowerCase() != "png")
+        {
+            var error = "Tipe file : "+ FileExt+"\n\n";
+            error += "Tipe file tanda tangan harus tipe PNG.\n\n";
+            alert(error);
+			location.reload();
+            return false;
+        }
+        cekdimensi(this);
+    }
+</script>
+<script>
+var _URL = window.URL || window.webkitURL;
+$("#userfile,#paraf").change(function(e) {
+    var file, img;
+
+
+    if ((file = this.files[0])) {
+        img = new Image();
+        img.onload = function() {
+		if(this.width != this.height){
+            alert("ERROR! Dimensi gambar tidak valid.\nLebar: "+this.width + "\nTinggi: " + this.height+"\n\nSilakan pilih gambar dengan tinggi dan lebar yang sama (tinggi = lebar).");
+			location.reload();
+		}
+        };
+        img.src = _URL.createObjectURL(file);
+
+
+    }
+});
+</script>
+<script type="text/javascript">
+   function cekparaf(fieldObj)
+    {
+        var FileName  = fieldObj.value;
+        var FileExt = FileName.substr(FileName.lastIndexOf('.')+1);
+
+        if (FileExt.toLowerCase() != "png")
+        {
+            var error = "Tipe file : "+ FileExt+"\n\n";
+            error += "Tipe file paraf harus tipe PNG.\n\n";
+            alert(error);
+			location.reload();
+            return false;
+        }
+        return true;
+    }
+</script>
 <script type="text/javascript">
 $(function() { 	
 	
