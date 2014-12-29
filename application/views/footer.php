8f1577b45b9e8d4498459dd75b277438b28f3468
FeriLupin <lord_aftermorfed@yahoo.com>
2014-12-29 16:34:11 +0700

    Update best view

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
 	<!--/footer-->
     <div class="footer">
-      <small><?php echo $config['app_abbr'].' :: '.$config['app_name'].' @'.date('Y')?></small>
+      <small><?php echo $config['app_abbr'].' :: '.$config['app_name'].' @'.date('Y').'<br>(Best view at Google Chrome 26 or Firefox 3-0 above on 1024x768 resolution)'?></small>
     </div>
 
     </div><!--/container-fluid-->
